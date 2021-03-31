494bbfb440f3e75d5c6b7c429eb73534ed2e1dc0
eyedesign-shuo <81454721+eyedesign-shuo@users.noreply.github.com>
2021-04-01 03:07:57 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(function(){
+  /* トップへ戻るボタン */
+    let pagetop = $('#page_top');
+    // ボタン非表示
+    pagetop.hide();
+    // 100px スクロールしたらボタン表示
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 100) {
+        pagetop.fadeIn();
+      } else {
+        pagetop.fadeOut();
+      }
+    });
+    pagetop.on("click", function () {
+      $('body, html').animate({ scrollTop: 0 }, 500);
+      return false;
+    });
+
+});
